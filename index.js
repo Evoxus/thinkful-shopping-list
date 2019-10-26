fed0d93199c1b15481fb09325a5965fc3ada46d0
Michael Ploughman <michael@michaelhploughman.com>
2019-10-26 13:25:14 -0600

    App functionallity requirements complete

---
+++ index.js
@@ -0,0 +1,33 @@
+$(function(){
+
+    // Add new item
+    $('#js-shopping-list-form').submit(e => {
+
+        e.preventDefault();
+
+        let newItem = $('#shopping-list-entry').val();
+
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+    
+    });
+
+    // Check item as complete
+    $('.shopping-list').on('click', '.shopping-item-toggle', function() {
+      $(this.parentElement.parentElement.children[0]).toggleClass('shopping-item__checked');
+    });
+    
+    // Delete item
+    $('.shopping-list').on('click', '.shopping-item-delete', function() {
+      $(this.parentElement.parentElement).remove();
+    });
+});
